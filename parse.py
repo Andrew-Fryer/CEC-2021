aa59b63306d1d838a4efdea5d6939f4c304a101f
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-27 12:27:07 -0500

    make zone first col

--- parse.py
+++ parse.py
@@ -11,21 +11,21 @@ total_zones = nb_zones.copy() + ['z8', 'z9', 'z10', 'z11']
 # penalty_values = [[float(x) for x in y.split(',')] for y in open('Information/PenaltyValues.csv', 'r').readlines()]
 
 penalty_values = pd.read_csv('Information/PenaltyValues.csv', header=None, names=total_zones.copy())
-penalty_values['zone'] = total_zones.copy()
+penalty_values.insert(loc=0, column='zone', value=total_zones.copy())
 
 plant_production_rates = pd.read_csv('Information/PlantProductionRates.csv', names=['thermal', 'nuclear', 'combustion', 'hydro', 'wind'])
-plant_production_rates['zone'] = nb_zones.copy()
+plant_production_rates.insert(loc=0, column='zone', value=nb_zones.copy())
 
 trend_2015 = pd.read_csv('PastYearData/NBTrend2015.csv', header=None, names=nb_zones.copy())
-trend_2015['months'] = months.copy()
+trend_2015.insert(loc=0, column='months', value=months.copy())
 
 trend_2016 = pd.read_csv('PastYearData/NBTrend2016.csv', header=None, names=nb_zones.copy())
-trend_2016['months'] = months.copy()
+trend_2016.insert(loc=0, column='months', value=months.copy())
 
 trend_2017 = pd.read_csv('PastYearData/NBTrend2017.csv', header=None, names=nb_zones.copy())
-trend_2017['months'] = months.copy()
+trend_2017.insert(loc=0, column='months', value=months.copy())
 
 trend_2018 = pd.read_csv('PastYearData/NBTrend2018.csv', header=None, names=nb_zones.copy())
-trend_2018['months'] = months.copy()
+trend_2018.insert(loc=0, column='months', value=months.copy())
 
 pass
