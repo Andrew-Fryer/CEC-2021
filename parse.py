19093f6c3fc2f12aa036b4fbd2d66513408624a2
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-27 12:00:46 -0500

    input data

---
+++ parse.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+incentive_rates = open('Information/IncentiveRates.csv', 'r').readlines()[0].split(',')
+[emission_tax, non_emission_tax] = [0.015, 0.009] # [float(x) for x in incentive_rates]
+
+# penalty_values = [[float(x) for x in y.split(',')] for y in open('Information/PenaltyValues.csv', 'r').readlines()]
+
+penalty_values = pd.read_csv('Information/PenaltyValues.csv', header=None)
+
+plant_production_rates = pd.read_csv('Information/PlantProductionRates.csv', names=['thermal', 'nuclear', 'combustion', 'hydro', 'wind']) 
+
+trend_2015 = pd.read_csv('PastYearData/NBTrend2015.csv', header=None)
+trend_2016 = pd.read_csv('PastYearData/NBTrend2016.csv', header=None)
+trend_2017 = pd.read_csv('PastYearData/NBTrend2017.csv', header=None)
+trend_2018 = pd.read_csv('PastYearData/NBTrend2018.csv', header=None)
+
+pass
