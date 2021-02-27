bda90ac5005dffff0d7f64120e16236871093379
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-27 12:07:41 -0500

    months to trends

--- parse.py
+++ parse.py
@@ -9,9 +9,17 @@ penalty_values = pd.read_csv('Information/PenaltyValues.csv', header=None)
 
 plant_production_rates = pd.read_csv('Information/PlantProductionRates.csv', names=['thermal', 'nuclear', 'combustion', 'hydro', 'wind']) 
 
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 trend_2015 = pd.read_csv('PastYearData/NBTrend2015.csv', header=None)
+trend_2015['months'] = months.copy()
+
 trend_2016 = pd.read_csv('PastYearData/NBTrend2016.csv', header=None)
+trend_2016['months'] = months.copy()
+
 trend_2017 = pd.read_csv('PastYearData/NBTrend2017.csv', header=None)
+trend_2017['months'] = months.copy()
+
 trend_2018 = pd.read_csv('PastYearData/NBTrend2018.csv', header=None)
+trend_2018['months'] = months.copy()
 
 pass
