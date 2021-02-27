5d747274b70f842cbf8e9b20601eb8a9e75c4e22
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-27 12:42:24 -0500

    normalize prod rates

--- parse.py
+++ parse.py
@@ -5,17 +5,19 @@ months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 
 nb_zones = ['z1', 'z2', 'z3', 'z4', 'z5', 'z6', 'z7']
 total_zones = nb_zones.copy() + ['z8', 'z9', 'z10', 'z11']
 
-#incentive_rates = [''.join([x for x in s if x.isalnum()]) for s in open('Information/IncentiveRates.csv', 'r').readlines()[0].split(',')]
-incentive_rates = pd.read_csv('Information/IncentiveRates.csv', header=None)
-[emission_tax, non_emission_tax] = incentive_rates.values[0]
+energy_types = ['thermal', 'nuclear', 'combustion', 'hydro', 'wind']
 
-# penalty_values = [[float(x) for x in y.split(',')] for y in open('Information/PenaltyValues.csv', 'r').readlines()]
+incentive_rates = pd.read_csv('Information/IncentiveRates.csv', header=None)
+[emission_tax, non_emission_tax] = incentive_rates.values[0] # already in kWh
 
 penalty_values = pd.read_csv('Information/PenaltyValues.csv', header=None, names=total_zones.copy())
 penalty_values.insert(loc=0, column='zone', value=total_zones.copy())
 
-plant_production_rates = pd.read_csv('Information/PlantProductionRates.csv', names=['thermal', 'nuclear', 'combustion', 'hydro', 'wind'])
+plant_production_rates = pd.read_csv('Information/PlantProductionRates.csv', names=energy_types.copy())
 plant_production_rates.insert(loc=0, column='zone', value=nb_zones.copy())
+# normalize values from MW to kW
+for e_type in energy_types:
+    plant_production_rates[e_type] = plant_production_rates[e_type] * 1000
 
 trend_2015 = pd.read_csv('PastYearData/NBTrend2015.csv', header=None, names=nb_zones.copy())
 trend_2015.insert(loc=0, column='months', value=months.copy())
