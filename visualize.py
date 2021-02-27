64bdf5a5718b40a069faf1687cf56836f3874041
joegr <joe.grosso@cogeco.ca>
2021-02-27 14:41:20 -0500

    Complete visualization code

--- visualize.py
+++ visualize.py
@@ -11,36 +11,38 @@ from parse import *
 energy_trends = [trend_2015, trend_2016, trend_2017, trend_2018]
 all_energy_data = pd.concat(energy_trends).reset_index().drop(labels=["index"], axis=1)
 
-print(all_energy_data)
-# print(penalty_values)
-# print(plant_production_rates)
-# print(emission_tax)
-# print(non_emission_tax)
-
-
-# get all of the energy from one of the 
-
-
-
-
 dates = [' '.join(i) for i in zip(all_energy_data["months"],all_energy_data["year"].map(str))]
 
 fig = go.Figure()
-
-for zone in all_energy_data.drop(labels=["year", "months"], axis=1):
-    fig.add_traces([go.Scatter(x=dates, y=all_energy_data[zone], name=zone)])
-
-
-
-fig.update_layout(template="ggplot2")
-fig.show()
-
 predConsumption = power_consumption_predict.get_predicted_power_usage(2019)
+colours =   ["#F8B195",  
+            "#F67280",
+            "#C06C84",
+            "#6C5B7B",
+            "#355C7D", 
+            "#99B898",
+            "#547980"]
+newMonthLabels = [f"{month} 2019" for month in months]
 
-print(predConsumption)
+actual_energy = all_energy_data.drop(labels=["year", "months"], axis=1)
 
-for zone in predConsumption:
-    fig.add_traces([go.Scatter(x=predConsumption.index, y=predConsumption[zone], name=zone)])
+for zone, index, colour in zip(actual_energy, predConsumption, colours):
+    actual = go.Scatter(x=dates, 
+                        y=all_energy_data[zone],
+                        name=f'{zone} Actual', 
+                        line=dict(color=colour))
+    predicted = go.Scatter(x=newMonthLabels, 
+                           y=predConsumption[index], 
+                           name=f'{zone} Predicted', 
+                           line=dict(color=colour, dash='dash'),
+                           mode="lines")
+    fig.add_traces([actual, predicted])
 
-fig.update_layout(template="ggplot2")
+
+
+fig.update_layout(template="ggplot2",
+                  title="Prediction of Power Usage Per Month By Zone",
+                  xaxis_title="Month and Year",
+                  yaxis_title="Power Requirements (GWh)",
+                  legend_title="Legend Title")
 fig.show()
