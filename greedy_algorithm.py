9cf9aa551c887f62e88248e19ce019ac26a1d053
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-27 18:01:00 -0500

    greedy runs

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -18,15 +18,14 @@ for energy_type in ['thermal', 'combustion']:
     demand_df[energy_type]= non_emission_tax
     demand_df[energy_type + '_usage'] = 0
 
-'''
-    zone, demand, z0_penalty, z0_usage, z1_penalty, z1_usage, ... hydro_penalty, hydro_usage, cost_function
-'''
-
-# Normalize by environmental incentives
-''' this requires having cols for z0_nuclear, z0_hydro, etc...'''
-#demand_df[]
-
 supply_df = plant_production_rates.copy(deep=True).set_index('zone').unstack()
+supply_dict = supply_df.T.to_dict()
+supply = [{'zone': t[0], 'energy_type': t[1], 'supply': supply_dict[t]} for t in supply_dict]
+
+def get_penalty_factor(current_zone_index, energy_type, other_zone):
+    assert(energy_type in energy_types)
+    #if(energy_type in ['nuclear', 'hydro', 'wind']):
+    return penalty_values[other_zone][zone_index]
 
 # for each month:
 # First, try to distribute power most efficiently
@@ -36,28 +35,19 @@ cost = 0
 for zone_index in range(len(nb_zones)):
     demand = demand_df['demand'][zone_index]
 
-    def helper(t):
-        energy_type = t[0][0]
-        zone = t[0][1]
-        penalty_factor = penalty_values[zone][zone_index] # todo account for tax thing
-        return (energy_type, zone, t[1], penalty_factor)
+    sources = [(t, get_penalty_factor(zone_index, t[0], t[1])) for t in supply_dict]
 
-    sources = [helper(t) for t in supply_df.T.to_dict().items()]
-
-    sources = sorted(sources, key=lambda t: t[3])
+    sources = sorted(sources, key=lambda t: t[1])
 
     for source in sources:
-        energy_type, zone, supply, penalty_factor = source
-        usage = min(demand, supply)
+        t, penalty_factor = source
+        energy_type, zone = t
+        usage = min(demand, supply_dict[t])
         #demand_df.at[name + '_usage', zone_index] = usage
         demand -= usage
         cost += penalty_factor * usage
-    #assert(demand == 0)
-
-
-
-    
+    assert(demand == 0)
 
 # Finally, sell any leftover power
 
-
+pass
